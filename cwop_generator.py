9f2a9d25538406b7bcbc077c81a40bf9e9ec398f
tubaman21 <kc9hna@gmail.com>
2026-09-18 14:47:13 -0500

    Update cwop_generator.py

--- cwop_generator.py
+++ cwop_generator.py
@@ -549,7 +549,7 @@ def main():
         f'IconFile: 2, 15, 15, 8, 8, "{SKY_COVER_ICON_URL}"',
         "Font: 1, 11, 400, 0",
         "// Created by: Bryan J. Howell and Gemini",
-        "/ /Last updated: 9/18/26",
+        "// Last updated: 9/18/26",
         ""
     ]
 
