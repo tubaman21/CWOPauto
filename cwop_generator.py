4dfc1d7d355f7a5eff9872a0b7726963d23a3353
tubaman21 <kc9hna@gmail.com>
2026-09-19 08:06:55 -0500

    Update cwop_generator.py

--- cwop_generator.py
+++ cwop_generator.py
@@ -545,7 +545,7 @@ def main():
         "; Last Updated: 9/18/26",
         f'Title: CWOP Surface Observations ({run_time})',
         "Refresh: 5",
-        f'IconFile: 1, 30, 30, 15, 15, "{WIND_BARB_ICON_URL}"',
+        f'IconFile: 1, 30, 30, 15, 29, "{WIND_BARB_ICON_URL}"',
         f'IconFile: 2, 15, 15, 8, 8, "{SKY_COVER_ICON_URL}"',
         "Font: 1, 11, 400, 0",
         ""
